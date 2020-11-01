4c1aae5bef79a990c6e12c7cc944743d8c2f1c4d
wr-rainforest <contact@wrforest.com>
2020-11-01 12:10:13 +0900

    TT 삭제, 버전 체계 변경

--- Naver-Webtoon-Downloader-GUI/Properties/AssemblyInfo.cs
+++ Naver-Webtoon-Downloader-GUI/Properties/AssemblyInfo.cs
@@ -1,21 +1,5 @@
-﻿/*
-
-
-
-
-
-
-
-
-
-
-
-
-*/
-using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿using System.Reflection;
 using System.Runtime.InteropServices;
-using System.Resources;
 
 // 어셈블리에 대한 일반 정보는 다음 특성 집합을 통해 
 // 제어됩니다. 어셈블리와 관련된 정보를 수정하려면
@@ -47,5 +31,5 @@ using System.Resources;
 // 모든 값을 지정하거나 아래와 같이 '*'를 사용하여 빌드 번호 및 수정 번호를
 // 기본값으로 할 수 있습니다.
 
-[assembly: AssemblyVersion("0.1.107.6")]
+[assembly: AssemblyVersion("0.2.0")]
 
