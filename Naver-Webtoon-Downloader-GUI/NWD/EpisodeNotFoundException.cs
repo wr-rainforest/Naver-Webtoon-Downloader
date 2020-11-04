9129696e603980938a8bb0209b6ea28118046bcf
wr-rainforest <contact@wrforest.com>
2020-11-04 20:57:55 +0900

    Agent, Parser를 NaverWebtoonHttpClient에 통합

---
+++ Naver-Webtoon-Downloader-GUI/NWD/EpisodeNotFoundException.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WRforest.NWD
+{
+    public class EpisodeNotFoundException : Exception
+    {
+        public string TitleId { get; set; }
+        public int EpisodeNo { get; set; }
+
+        public EpisodeNotFoundException()
+        {
+
+        }
+
+        public EpisodeNotFoundException(string message) : base(message)
+        {
+        }
+
+        public EpisodeNotFoundException(string titleId, int episodeNo)
+        {
+            TitleId = titleId;
+            EpisodeNo = episodeNo;
+        }
+
+        public EpisodeNotFoundException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected EpisodeNotFoundException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
---
+++ Naver-Webtoon-Downloader-GUI/NWD/NaverWebtoonHttpClient.cs
@@ -0,0 +1,90 @@
+﻿
+using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace WRforest.NWD
+{
+    class NaverWebtoonHttpClient : HttpClient
+    {
+        public NaverWebtoonHttpClient() : base(new HttpClientHandler() { AllowAutoRedirect = false })
+        {
+            var version = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version;
+            DefaultRequestHeaders.Add("User-Agent",$"NaverWebtoonDownloader/{version.Major}.{version.Minor}");
+        }
+        private async Task<HtmlDocument> GetListPageDocumentAsync(string titleId)
+        {
+            var uri = $"https://comic.naver.com/webtoon/list.nhn?titleId={titleId}";
+            var responseString = await GetStringAsync(uri);
+            HtmlDocument document = new HtmlDocument();
+            document.LoadHtml(responseString);
+            return document;
+        }
+
+        private async Task<HtmlDocument> GetEpisodePageDocumentAsync(string titleId, int episodeNo)
+        {
+            var uri = $"https://comic.naver.com/webtoon/detail.nhn?titleId={titleId}&no={episodeNo}";
+            var response = await GetAsync(uri);
+            if(response.StatusCode==HttpStatusCode.Redirect)
+            {
+                throw new EpisodeNotFoundException(titleId, episodeNo);
+            }
+            var responseString = await GetStringAsync(uri);
+            HtmlDocument document = new HtmlDocument();
+            document.LoadHtml(responseString);
+            return document;
+        }
+
+        public async Task<string> GetWebtoonTitleAsync(string titleId)
+        {
+            var document = await GetListPageDocumentAsync(titleId);
+            var title = document.DocumentNode.SelectSingleNode("//*[@property=\"og:title\"]").Attributes["content"].Value;
+            return title;
+        }
+
+        public async Task<string> GetWebtoonWriterAsync(string titleId)
+        {
+            var document = await GetListPageDocumentAsync(titleId);
+            var writer = document.DocumentNode.SelectSingleNode("//*[@name=\"itemWriterId\"]").Attributes["value"].Value;
+            return writer;
+        }
+
+        public async Task<int> GetLatestEpisodeNoAsync(string titleId)
+        {
+            var document = await GetListPageDocumentAsync(titleId);
+            var relativeUri = document.DocumentNode.SelectSingleNode("//td[@class=\"title\"]/a").Attributes["href"].Value;
+            var absoluteUri = $"https://comic.naver.com{relativeUri}";
+            return int.Parse(HttpUtility.ParseQueryString(new Uri(absoluteUri).Query).Get("no"));
+        }
+
+        public async Task<string> GetEpisodeTitleAsync(string titleId, int episodeNo)
+        {
+            var document = await GetEpisodePageDocumentAsync(titleId, episodeNo);
+            return document.DocumentNode.SelectSingleNode("//*[@property=\"og:description\"]").Attributes["content"].Value;
+        }
+
+        public async Task<string> GetEpisodeDateAsync(string titleId, int episodeNo)
+        {
+            var document = await GetEpisodePageDocumentAsync(titleId, episodeNo);
+            return document.DocumentNode.SelectSingleNode("//*[@class=\"date\"]").InnerText;
+        }
+
+        public async Task<string[]> GetEpisodeImageUriListAsync(string titleId, int episodeNo)
+        {
+            var document = await GetEpisodePageDocumentAsync(titleId, episodeNo);
+            List<string> uris = new List<string>();
+            HtmlNodeCollection nodes = document.DocumentNode.SelectNodes("//*[@alt=\"comic content\"]");
+            for (int i = 0; i < nodes.Count; i++)
+            {
+                uris.Add(nodes[i].Attributes["src"].Value);
+            }
+            return uris.ToArray();
+        }
+    }
+}
