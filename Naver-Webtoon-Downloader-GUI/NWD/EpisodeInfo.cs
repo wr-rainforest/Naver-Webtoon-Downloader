875e95d3aa1bb20fdada5dbe01626e354555e8c6
wr-rainforest <contact@wrforest.com>
2020-11-04 21:14:39 +0900

    EpisodeInfo, WebtoonInfo 수정, 네임스페이스 변경

--- Naver-Webtoon-Downloader-GUI/NWD/EpisodeInfo.cs
+++ Naver-Webtoon-Downloader-GUI/NWD/EpisodeInfo.cs
@@ -4,35 +4,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using WRforest.NWD.Key;
 
-namespace WRforest.NWD.DataType
+namespace WRforest.NWD
 {
     class EpisodeInfo
     {
         /// <summary>
+        /// 웹툰 TitleId입니다.
+        /// </summary>
+        public string TitleId { get; private set; }
+
+        /// <summary>
         /// 회차 제목입니다.
         /// </summary>
-        [JsonProperty(PropertyName = "episode_title")]
-        public string EpisodeTitle { get; private set; }
+        public string Title { get; private set; }
 
         /// <summary>
         /// 회차 번호입니다. 1부터 시작합니다.
         /// </summary>
-        [JsonProperty(PropertyName = "episode_no")]
-        public int EpisodeNo { get; private set; }
+        public int No { get; private set; }
 
         /// <summary>
         /// 웹툰 등록일(yyyy.MM.dd)입니다.
         /// </summary>
-        [JsonProperty(PropertyName = "episode_date")]
-        public string EpisodeDate { get; private set; }
+        public string Date { get; private set; }
 
         /// <summary>
         /// 웹툰 이미지 Url 목록입니다.
         /// </summary>
-        [JsonProperty(PropertyName = "episode_image_urls")]
-        public string[] EpisodeImageUrls { get; private set; }
+        public string[] ImageUrls { get; private set; }
 
 
         /// <summary>
@@ -40,14 +40,15 @@ namespace WRforest.NWD.DataType
         /// </summary>
         /// <param name="title">회차 제목</param>
         /// <param name="episodeNo">에피소드 번호</param>
-        /// <param name="imageUrls"></param>
         /// <param name="date">웹툰 등록일</param>
-        public EpisodeInfo(EpisodeKey episodeKey,string title, string[] imageUrls, string date)
+        /// <param name="imageUrls"></param>
+        public EpisodeInfo(string titleId, int episodeNo ,string title, string date, string[] imageUrls)
         {
-            EpisodeTitle = title;
-            EpisodeNo = episodeKey.EpisodeNo;
-            EpisodeImageUrls = imageUrls;
-            EpisodeDate = date;
+            TitleId = titleId;
+            Title = title;
+            No = episodeNo;
+            Date = date;
+            ImageUrls = imageUrls;
         }
         public EpisodeInfo() { }
     }
---
+++ Naver-Webtoon-Downloader-GUI/NWD/WebtoonInfo.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace WRforest.NWD
+{
+    class WebtoonInfo
+    {
+        /// <summary>
+        /// 웹툰 제목입니다.
+        /// </summary>
+        public string Title { get; private set; }
+
+        /// <summary>
+        /// 웹툰 id입니다.
+        /// </summary>
+        public string TitleId { get; private set; }
+
+        /// <summary>
+        /// 웹툰작가 닉네임입니다.
+        /// </summary>
+        public string Writer { get; set; }
+
+        /// <summary>
+        /// 웹툰 장르입니다.
+        /// </summary>
+        public string Genre { get; private set; }
+
+        /// <summary>
+        /// 웹툰 설명입니다.
+        /// </summary>
+        public string Description { get; private set; }
+
+
+        /// <summary>
+        /// <seealso cref="WebtoonInfo"/>의 새 인스턴스를 초기화합니다.
+        /// </summary>
+        /// <param name="titleId"></param>
+        /// <param name="title"></param>
+        public WebtoonInfo(string titleId, string title, string writer, string genre, string description)
+        {
+            TitleId = titleId;
+            Title = title;
+            Writer = writer;
+            Genre = genre;
+            Description = description;
+        }
+
+    }
+}
