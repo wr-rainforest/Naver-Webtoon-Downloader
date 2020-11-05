a94686df2229e67064076a12d9f673deb3feb626
wr-rainforest <contact@wrforest.com>
2020-11-05 09:44:51 +0900

    로그인 메서드 추가, 구현x

--- Naver-Webtoon-Downloader/NaverWebtoonHttpClient.cs
+++ Naver-Webtoon-Downloader/NaverWebtoonHttpClient.cs
@@ -1,8 +1,12 @@
 ﻿using HtmlAgilityPack;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Security.Cryptography;
+using System.Text;
 using System.Threading.Tasks;
 using System.Web;
 
@@ -14,6 +18,60 @@ namespace wr_rainforest.Naver_Webtoon_Downloader
         {
             var version = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version;
             DefaultRequestHeaders.Add("User-Agent",$"NaverWebtoonDownloader/{version.Major}.{version.Minor}");
+        }
+        public async Task<bool> Login(string id, string password)
+        {
+            throw new NotImplementedException("구현되지 않은 기능");
+            var responseString = await GetStringAsync("https://nid.naver.com/login/ext/keys.nhn");
+            var sessionSplited = responseString.Split(',');
+            var sessionKey = sessionSplited[0];
+            var sessionName = sessionSplited[1];
+            var modulusKey = sessionSplited[2];
+            var exponentKey = sessionSplited[3];
+            var plainText = 
+                $"{Convert.ToChar(sessionKey.Length)}" +
+                $"{sessionKey}" +
+                $"{Convert.ToChar(id.Length)}" +
+                $"{id}" +
+                $"{Convert.ToChar(password.Length)}" +
+                $"{password}";
+            var plainBytes = Encoding.UTF8.GetBytes(plainText);
+            var rsaParams = new RSAParameters()
+            {
+                Modulus = Enumerable.Range(0, modulusKey.Length)
+                .Where(x => x % 2 == 0)
+                .Select(x => Convert.ToByte(modulusKey.Substring(x, 2), 16))
+                .ToArray(),
+                Exponent = Enumerable.Range(0, exponentKey.Length)
+                .Where(x => x % 2 == 0)
+                .Select(x => Convert.ToByte(exponentKey.Substring(x, 2), 16))
+                       .ToArray()
+            };
+            var rsa = new RSACryptoServiceProvider();
+            rsa.ImportParameters(rsaParams);
+            var encrytedBytes = rsa.Encrypt(plainBytes, false);
+            var encpw = BitConverter.ToString(encrytedBytes).Replace("-", "").ToLower();
+            var loginRequest = new HttpRequestMessage();
+            var keyValuePairs = new Dictionary<string, string>();
+            keyValuePairs.Add("localechange", encpw);
+            keyValuePairs.Add("encpw", encpw);
+            keyValuePairs.Add("enctp", "1");
+            keyValuePairs.Add("svctype", "1");
+            var bvsd = new 
+            {
+                uuid = "b7763c36-2a93-4a96-a4df-a85679a575e1-0",
+                encData= ""
+            };
+            keyValuePairs.Add("bvsd", JsonConvert.SerializeObject(bvsd));
+            keyValuePairs.Add("smart_LEVEL", "-1");
+            keyValuePairs.Add("encnm", sessionName);
+            keyValuePairs.Add("locale", "ko_KR");
+            keyValuePairs.Add("url", "https://www.naver.com");
+            keyValuePairs.Add("id", "");
+            keyValuePairs.Add("pw", "1");
+            loginRequest.Content = new FormUrlEncodedContent(keyValuePairs);
+            loginRequest.Headers.Add("Referer", "https://nid.naver.com/nidlogin.login");
+            //미구현
         }
         private async Task<HtmlDocument> GetListPageDocumentAsync(string titleId)
         {
