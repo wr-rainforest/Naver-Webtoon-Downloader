5b92d0672bceb555d692a75b5b1dc08332a67e8a
wr-rainforest <contact@wrforest.com>
2020-11-08 21:16:03 +0900

    프로젝트 수정

---
+++ Naver-Webtoon-Downloader/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using NaverWebtoonDownloader.CoreLib;
+
+namespace NaverWebtoonDownloader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
